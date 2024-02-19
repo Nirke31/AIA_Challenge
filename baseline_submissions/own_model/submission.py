53b09ee7f2440e96ed57a6e0e4da1f9ee3f813f5
Niklas Mika <niklas@mika.gl>
2024-02-19 21:02:41 +0100

    restructed everything a bit

--- baseline_submissions/own_model/submission.py
+++ baseline_submissions/own_model/submission.py
@@ -7,8 +7,12 @@ from torch.utils.data import DataLoader
 import torchvision.models as models
 from torch.profiler import profile, record_function, ProfilerActivity
 from timeit import default_timer as timer
+import matplotlib.pyplot as plt
+
+from typing import List
+
 from myModel import Seq2SeqTransformer, create_mask
-from dataset_manip import MyDataset, load_data, convert_tgts_for_eval
+from dataset_manip import MyDataset, load_data, convert_tgts_for_eval, split_train_test
 from baseline_submissions.evaluation import NodeDetectionEvaluator
 
 # torch.autograd.set_detect_anomaly(True)
@@ -26,7 +30,60 @@ SRC_PADDING_VEC = torch.Tensor([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 1
 TGT_PADDING_VEC = torch.Tensor([15])
 
 
-def train_epoch(dl: DataLoader, model: torch.nn.Module, optimizer: torch.optim.Optimizer, ):
+def load_model_and_datasets(amount: int = 10):
+    print("Loading Dataframe")
+    data: pd.DataFrame = load_data(TRAIN_DATA_PATH, TRAIN_LABEL_PATH, amount)
+    data = data[FEATURES_AND_TGT]
+    print("Dataframe load done")
+
+    # get Dataset
+    train_df, test_df = split_train_test(data)
+    ds_train = MyDataset(train_df)
+    ds_test = MyDataset(test_df)
+    print("Own Dataset created")
+
+    # Create transformer
+    transformer = Seq2SeqTransformer(NUM_ENCODER_LAYERS, NUM_DECODER_LAYERS, NHEAD, SRC_SIZE,
+                                     TGT_SIZE, FF_SIZE, DROPOUT)
+    # idk if I really need this. shouldn't torch do this already?
+    for p in transformer.parameters():
+        if p.dim() > 1:
+            torch.nn.init.xavier_uniform_(p)
+    transformer.to(DEVICE)
+    print("Transformer created")
+
+    loss_fn = torch.nn.CrossEntropyLoss()  # ignore_index=TBD ?
+    optimizer = torch.optim.Adam(transformer.parameters(), lr=LR, betas=BETAS, eps=EPS, weight_decay=WEIGHT_DECAY)
+
+    return ds_train, ds_test, transformer, loss_fn, optimizer
+
+
+def train(ds: MyDataset, transformer: torch.nn.Module, optimizer: torch.optim.Optimizer, store_model: bool = True) -> \
+        List[float]:
+    # Train
+
+    # create Dataloader
+    dl = DataLoader(ds, batch_size=BATCH_SIZE)
+    print("Dataloader created")
+
+    loss = []
+    print("Starting Training: ----------------------------------------------------")
+    for epoch in range(1, NUM_EPOCHS + 1):
+        start_time = timer()
+        train_loss = train_epoch(dl, transformer, optimizer)
+        end_time = timer()
+        print(f"Epoch: {epoch}, Train loss: {train_loss:.3f}, Epoch time = {(end_time - start_time):.3f}s")
+        loss.append(train_loss)
+    print("Training done")
+
+    if store_model:
+        print("Storing model")
+        torch.save(transformer.state_dict(), TRAINED_MODEL_PATH)
+
+    return loss
+
+
+def train_epoch(dl: DataLoader, model: torch.nn.Module, optimizer: torch.optim.Optimizer):
     model.train()
     losses = 0
 
@@ -57,7 +114,7 @@ def train_epoch(dl: DataLoader, model: torch.nn.Module, optimizer: torch.optim.O
 def simple_eval(ds: MyDataset, model: torch.nn.Module):
     model.eval()
 
-    tgt_dict = ds.tgt_dict
+    tgt_dict = ds.tgt_dict_int_to_str
     dl_eval = DataLoader(ds, batch_size=1)
 
     pred_all = []
@@ -84,70 +141,71 @@ def simple_eval(ds: MyDataset, model: torch.nn.Module):
     # tgt_df.to_csv("tgt.csv")
 
     evaluator = NodeDetectionEvaluator(tgt_df, pred_df, 6)
-    precision, recall, f2, rmse = evaluator.score(debug=False)
-    print(f'Precision: {precision:.2f}')
-    print(f'Recall: {recall:.2f}')
-    print(f'F2: {f2:.2f}')
-    print(f'RMSE: {rmse:.2f}')
-    return
+    return evaluator
 
 
-# DEFINES
-NUM_EPOCHS = 10
+# Learning settings
+NUM_EPOCHS = 100
+BATCH_SIZE = 1
+FEATURES_AND_TGT = [
+    "Timestamp",
+    "Eccentricity",
+    "Semimajor Axis (m)",
+    "Inclination (deg)",
+    "RAAN (deg)",
+    "Argument of Periapsis (deg)",
+    "True Anomaly (deg)",
+    "Latitude (deg)",
+    "Longitude (deg)",
+    "Altitude (m)",
+    "X (m)",
+    "Y (m)",
+    "Z (m)",
+    "Vx (m/s)",
+    "Vy (m/s)",
+    "Vz (m/s)",
+    "EW/NS"
+]
+# Transformer settings
 NUM_ENCODER_LAYERS = 3
 NUM_DECODER_LAYERS = 3
 NHEAD = 8
 SRC_SIZE = 16
-TGT_SIZE = -1  # updated in code down below
+TGT_SIZE = 32
 FF_SIZE = 512
 DROPOUT = 0.1
+# Optimizer settings
 LR = 0.0001
 BETAS = (0.9, 0.98)
 EPS = 1e-9
 WEIGHT_DECAY = 0  # For now keep as ADAM, default
+# User settings
+LOAD_MODEL = False
+TRAINED_MODEL_PATH = Path('./trained_model/model.pkl')
+TRAIN_DATA_PATH = Path("//wsl$/Ubuntu/home/backwelle/splid-devkit/dataset/phase_1_v2/train")
+TRAIN_LABEL_PATH = Path("//wsl$/Ubuntu/home/backwelle/splid-devkit/dataset/phase_1_v2/train_labels.csv")
 
 if __name__ == "__main__":
-    train_data_str = "//wsl$/Ubuntu/home/backwelle/splid-devkit/dataset/phase_1_v2/train"
-    train_label_str = "//wsl$/Ubuntu/home/backwelle/splid-devkit/dataset/phase_1_v2/train_labels.csv"
-    print("Loading Dataframe")
-    data: pd.DataFrame = load_data(train_data_str, train_label_str)
-    print("Dataframe load done")
 
-    # get Dataset
-    ds = MyDataset(data)
-    TGT_SIZE = len(ds.tgt_dict)  # number of different targets
-    print("Own Dataset created")
+    # get everything
+    num_csv_sets = 10
+    ds_train, ds_test, transformer, loss_fn, optimizer = load_model_and_datasets(num_csv_sets)
 
-    # create Dataloader
-    dl = DataLoader(ds, batch_size=1)
-    print("Dataloader created")
-
-    # Create transformer
-    transformer = Seq2SeqTransformer(NUM_ENCODER_LAYERS, NUM_DECODER_LAYERS, NHEAD, SRC_SIZE,
-                                     TGT_SIZE, FF_SIZE, DROPOUT)
-    # idk if I really need this. shouldn't torch do this already?
-    for p in transformer.parameters():
-        if p.dim() > 1:
-            torch.nn.init.xavier_uniform_(p)
-    transformer.to(DEVICE)
-    print("Transformer created")
-
-    loss_fn = torch.nn.CrossEntropyLoss()  # ignore_index=TBD ?
-
-    optimizer = torch.optim.Adam(transformer.parameters(), lr=LR, betas=BETAS, eps=EPS, weight_decay=WEIGHT_DECAY)
-
-    # Train
-    print("Starting Training: ----------------------------------------------------")
-    for epoch in range(1, NUM_EPOCHS + 1):
-        start_time = timer()
-        train_loss = train_epoch(dl, transformer, optimizer)
-        end_time = timer()
-        print(f"Epoch: {epoch}, Train loss: {train_loss:.3f}, Epoch time = {(end_time - start_time):.3f}s")
-
-    print("Training done")
-
-    print("Storing model")
-    torch.save(transformer.state_dict(), Path('./trained_model/model.pkl'))
+    # train or load
+    if LOAD_MODEL:
+        transformer.load_state_dict(torch.load(TRAINED_MODEL_PATH))
+    else:
+        losses = train(ds_train, transformer, optimizer)
+        # plt.plot(losses)
+        # plt.show()
 
     print("Evaluation...")
-    simple_eval(ds, model=transformer)
+    evaluatinator = simple_eval(ds_test, model=transformer)
+
+    precision, recall, f2, rmse = evaluatinator.score(debug=False)
+    print(f'Precision: {precision:.2f}')
+    print(f'Recall: {recall:.2f}')
+    print(f'F2: {f2:.2f}')
+    print(f'RMSE: {rmse:.2f}')
+
+    # evaluatinator.plot(object_id=1157)
