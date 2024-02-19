d445930eb12db10c8593c35c5b05c3b644458073
Niklas Mika <niklas@mika.gl>
2024-02-19 21:02:53 +0100

    this and that

--- baseline_submissions/own_model/test.py
+++ baseline_submissions/own_model/test.py
@@ -1,8 +1,41 @@
+import time
+
 import pandas as pd
 import torch
+from timeit import default_timer as timer
 from torch import Tensor
 import torch.nn as nn
+from dataset_manip import load_data, split_train_test
 
+# https://eval.ai/web/challenges/challenge-page/2164/overview
 
-print(torch.cuda.get_device_capability()[0])
+train_data_str = "//wsl$/Ubuntu/home/backwelle/splid-devkit/dataset/phase_1_v2/train"
+train_label_str = "//wsl$/Ubuntu/home/backwelle/splid-devkit/dataset/phase_1_v2/train_labels.csv"
 
+# print("Load whole dataset")
+# load1 = timer()
+# whole_df = load_data(train_data_str, train_label_str, 99999999)
+# time_normal_load = timer() - load1
+#
+# print("Store...")
+# whole_df.to_csv('./dataset/all_data.csv', index=False)
+# print("Load it again")
+# load2 = timer()
+# print(load_data(train_data_str, train_label_str))
+# time_all_load = timer() - load2
+#
+# print(f"First load: {time_normal_load}, Second load: {time_all_load}")
+
+# df = load_data(train_data_str, train_label_str, 3)
+#
+# split_train_test(df)
+
+labels = pd.read_csv('./dataset/all_data.csv', usecols=['EW/NS'])
+
+unique_df: pd.DataFrame = labels.drop_duplicates()
+
+unique_df = unique_df.reset_index()
+d = unique_df.to_dict()["EW/NS"]
+
+print(d)
+print(len(d))
