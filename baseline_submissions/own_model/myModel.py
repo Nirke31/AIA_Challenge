8a7dd537f3887f1da0655319fa6ee9dd465e877a
Niklas Mika <niklas@mika.gl>
2024-02-20 12:35:21 +0100

    took out unnecessary masks to reduce memory

--- baseline_submissions/own_model/myModel.py
+++ baseline_submissions/own_model/myModel.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import torch
 from torch import Tensor
 import torch.nn as nn
@@ -27,7 +29,7 @@ def create_mask(src: torch.Tensor, tgt: torch.Tensor, num_heads: int, src_pad_ve
 
     # src and tgt mask, masking the src and tgt sequence. Has to be of shape: batch len * NHEAD, seq len, seq len
     # I think I do not have to add the padding here because already in padding_mask?
-    src_mask = torch.zeros((batch_len * num_heads, src_seq_len, src_seq_len), device=device).type(torch.bool)
+    # src_mask = torch.zeros((batch_len * num_heads, src_seq_len, src_seq_len), device=device).type(torch.bool)
     tgt_mask = torch.nn.Transformer.generate_square_subsequent_mask(tgt_seq_len, device=device)
     tgt_mask = tgt_mask.unsqueeze(0).repeat(batch_len * num_heads, 1, 1)
 
@@ -36,9 +38,9 @@ def create_mask(src: torch.Tensor, tgt: torch.Tensor, num_heads: int, src_pad_ve
     tgt_padding_mask = (tgt == tgt_pad_vector.view(1, 1, tgt_num_features)).all(dim=2).to(device=device)
 
     # Create memory mask, idk what it does yet so just zeros(mask is irrelevant)? example used same as source
-    memory_mask = torch.zeros((tgt_seq_len, src_seq_len), device=device).type(torch.bool)
-    memory_padding_mask = src_padding_mask
-    return src_mask, tgt_mask, memory_mask, src_padding_mask, tgt_padding_mask, memory_padding_mask
+    # memory_mask = torch.zeros((tgt_seq_len, src_seq_len), device=device).type(torch.bool)
+    # memory_padding_mask = src_padding_mask
+    return tgt_mask, src_padding_mask, tgt_padding_mask,
 
 
 # Copied from pytorch. I am unsure why we are not directly using the embedding by multiplying it with the squareroot.
@@ -46,7 +48,7 @@ def create_mask(src: torch.Tensor, tgt: torch.Tensor, num_heads: int, src_pad_ve
 class TokenEmbedding(nn.Module):
     def __init__(self, vocab_size: int, emb_size):
         super(TokenEmbedding, self).__init__()
-        self.embedding = nn.Embedding(vocab_size, emb_size)
+        self.embedding = nn.Embedding(vocab_size, emb_size, padding_idx=34)  # TODO: GET THIS FROM SOMEWHERE
         self.emb_size = emb_size
 
     def forward(self, tokens: Tensor):
@@ -86,12 +88,12 @@ class Seq2SeqTransformer(nn.Module):
 
     def forward(self, src: Tensor,
                 tgt: Tensor,
-                src_mask: Tensor,
-                tgt_mask: Tensor,
-                memory_mask: Tensor,
-                src_padding_mask: Tensor,
-                tgt_padding_mask: Tensor,
-                memory_padding_mask: Tensor):
+                src_mask: Optional[Tensor] = None,
+                tgt_mask: Optional[Tensor] = None,
+                memory_mask: Optional[Tensor] = None,
+                src_padding_mask: Optional[Tensor] = None,
+                tgt_padding_mask: Optional[Tensor] = None,
+                memory_padding_mask: Optional[Tensor] = None):
 
         # normalise src and get tgt embedding
         src = self.src_normalisation(src)
