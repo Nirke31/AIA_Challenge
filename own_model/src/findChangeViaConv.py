7d796062f1575158158ce4f62d79490182b73ca2
Niklas Mika <niklas@mika.gl>
2024-03-04 21:22:44 +0100

    with weighted random sampler

--- own_model/src/findChangeViaConv.py
+++ own_model/src/findChangeViaConv.py
@@ -1,4 +1,5 @@
 import math
+import pickle
 from pathlib import Path
 from timeit import default_timer as timer
 
@@ -15,7 +16,7 @@ from sklearn.metrics import fbeta_score, make_scorer
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.inspection import permutation_importance
 from sklearn.preprocessing import StandardScaler
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, WeightedRandomSampler
 
 from own_model.src.dataset_manip import load_data, state_change_eval, MyDataset, ChangePointDataset
 from own_model.src.myModel import LitChangePointClassifier
@@ -53,65 +54,68 @@ BASE_FEATURES_NS = ["Eccentricity",
 
 TRAIN_TEST_RATIO = 0.8
 RANDOM_STATE = 42
-NUM_WORKERS = 4
-EPOCHS = 500
-BATCH_SIZE = 100
+NUM_WORKERS = 2
+EPOCHS = 200
+BATCH_SIZE = 1
 SHUFFLE_DATA = False
-WINDOW_SIZE = 5
+WINDOW_SIZE = 25
 DIRECTION = "EW"
-NUM_CSV_SETS = 1000
+NUM_CSV_SETS = -1
 
 L.seed_everything(RANDOM_STATE, workers=True)
 
+
+# https://github.com/geekfeiw/Multi-Scale-1D-ResNet/blob/master/figs/network.png
+# https://github.com/vdumoulin/conv_arithmetic/blob/master/README.md
 
 def main():
     df: pd.DataFrame = load_data(TRAIN_DATA_PATH, TRAIN_LABEL_PATH, amount=NUM_CSV_SETS)
     # manually remove the change point at time index 0. We know that there is a time change so we do not have to try
     # and predict it
-    df.loc[df["TimeIndex"] == 0, "EW"] = 0
-    df.loc[df["TimeIndex"] == 0, "NS"] = 0
+    df.loc[df["TimeIndex"] == 0, "EW"] = 0.0
+    df.loc[df["TimeIndex"] == 0, "NS"] = 0.0
 
     # features, leaves room for potential feature engineering
     features = BASE_FEATURES_EW if DIRECTION == "EW" else BASE_FEATURES_NS
+    # Scale, yes I am leaking, would be way more complicated to not leak. Maybe change later anyways?
+    scaler = StandardScaler()
+    df.loc[:, features] = pd.DataFrame(scaler.fit_transform(df.loc[:, features]), index=df.index, columns=features)
 
-    # label creation
-    labels = df.loc[:, DIRECTION]
-    # shuffle targets
-    labels = labels.sample(frac=1)
-    labels.reset_index(drop=True, inplace=True)
     print("Dataset loaded")
 
-    # generate train/val/test data
-    mask = np.random.rand(len(df)) < TRAIN_TEST_RATIO
-
-    df_train = df[mask]
-    df_test = df[~mask]
-    label_train = labels[mask]
-    label_test = labels[~mask]
-
-    # Scale
-    scaler = StandardScaler()
-    df_train.loc[:, features] = pd.DataFrame(scaler.fit_transform(df_train.loc[:, features]),
-                                             index=df_train.index, columns=features)
-    df_test.loc[:, features] = pd.DataFrame(scaler.transform(df_test.loc[:, features]),
-                                            index=df_test.index, columns=features)
+    # split train test, DATA IS SHUFFLED!
+    labels = df.loc[:, DIRECTION].to_numpy()
+    indices = np.arange(labels.shape[0])
+    idx_train, idx_val = train_test_split(indices, train_size=TRAIN_TEST_RATIO, stratify=labels)
 
     # load dataset and dataloader
-    ds_train = ChangePointDataset(df_train.loc[:, features], label_train, WINDOW_SIZE, negative_to_positive_ration=100)
-    ds_test = ChangePointDataset(df_test.loc[:, features], label_test, WINDOW_SIZE, negative_to_positive_ration=200)
+    ds_train = ChangePointDataset(df.loc[:, features], labels, idx_train, WINDOW_SIZE)
+    ds_test = ChangePointDataset(df.loc[:, features], labels, idx_val, WINDOW_SIZE)
+
+    # Create random sampler to sample more positives than negatives
+    num_samples = ds_train.tgt.shape[0]
+    num_pos = ds_train.tgt.sum()
+    num_neg = num_samples - ds_train.tgt.sum()
+    class_counts = {1: num_pos, 0: num_neg}
+    # each sample is assigned a weight based on its class
+    weights = [1 / class_counts[i] for i in ds_train.tgt]
+    sampler = WeightedRandomSampler(weights=weights, num_samples=num_samples)
+
     dataloader_train = DataLoader(ds_train, batch_size=BATCH_SIZE, shuffle=SHUFFLE_DATA, num_workers=NUM_WORKERS,
-                                  persistent_workers=True, pin_memory=True)
+                                  persistent_workers=True, pin_memory=False, sampler=sampler)
+    # For val (and test) we do not need a sampler because we just look at all values once
     dataloader_val = DataLoader(ds_test, batch_size=BATCH_SIZE, shuffle=SHUFFLE_DATA, num_workers=NUM_WORKERS,
-                                persistent_workers=True, pin_memory=True)
-    dataloader_test = DataLoader(ds_test, batch_size=BATCH_SIZE, shuffle=SHUFFLE_DATA, num_workers=NUM_WORKERS)  # bit of leftover
+                                persistent_workers=True, pin_memory=False)
+    dataloader_test = DataLoader(ds_test, batch_size=BATCH_SIZE, shuffle=SHUFFLE_DATA,
+                                 num_workers=NUM_WORKERS)  # bit of leftover
 
     print("Start fitting...")
 
     # get actual model
-    model = LitChangePointClassifier(WINDOW_SIZE, len(features))
-    early_stop_callback = EarlyStopping(monitor="val_f2", mode="max", patience=5)
+    early_stop_callback = EarlyStopping(monitor="val_BinaryFBetaScore", mode="max", patience=5)
     trainer = L.Trainer(max_epochs=EPOCHS, enable_progress_bar=True,
                         callbacks=[early_stop_callback], check_val_every_n_epoch=10, accumulate_grad_batches=5)
+    model = LitChangePointClassifier(WINDOW_SIZE, len(features))
     trainer.fit(model=model, train_dataloaders=dataloader_train, val_dataloaders=dataloader_val)
     trainer.test(model=model, dataloaders=dataloader_test)
 
