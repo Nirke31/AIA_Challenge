ffa0d337e2cd35cbbc19194be85f4c5211135c8e
Niklas Mika <niklas@mika.gl>
2024-03-15 23:59:54 +0100

    restructure and added new feature of findChange. Upload still ongoing. Unsure if restructure was without errors

--- own_model/src/submission.py
+++ own_model/src/submission.py
@@ -4,6 +4,7 @@ import time
 
 import torch
 import lightning as L
+from scipy.signal import savgol_filter, find_peaks
 from torch import Tensor
 from torch.utils.data import DataLoader
 from sklearn.preprocessing import StandardScaler
@@ -32,6 +33,7 @@ RF_BASE_FEATURES_EW = [
     "Longitude (deg)",
 ]
 RF_BASE_FEATURES_NS = [
+    "Eccentricity",
     "Inclination (deg)",
     "RAAN (deg)",
     "Longitude (deg)",
@@ -39,12 +41,12 @@ RF_BASE_FEATURES_NS = [
 
 ENGINEERED_FEATURES_EW = {
     ("std", lambda x: x.rolling(window=WINDOW_SIZE).std()):
-        ["Semimajor Axis (m)", "Altitude (m)", "Eccentricity"]
+        ["Semimajor Axis (m)", "Altitude (m)", "Eccentricity", "Inclination (deg)"]
 
 }
 ENGINEERED_FEATURES_NS = {
     ("std", lambda x: x.rolling(window=WINDOW_SIZE).std()):
-        ["Semimajor Axis (m)", "Altitude (m)"]
+        ["Semimajor Axis (m)", "Altitude (m)", "Eccentricity", "Inclination (deg)"]
 }
 
 CLASSIFIER_FEATURES = ["Eccentricity",
@@ -79,6 +81,69 @@ DEG_FEATURES = [
 ]
 
 
+def get_peaks(x: pd.DataFrame):
+    objectID = x.index.get_level_values(0).unique()
+    x["smoothed"] = savgol_filter(x["Inclination (deg)_std"], 13, 3)
+
+    inc_std_average = x["smoothed"].median()
+
+    peaks, _ = find_peaks(x["smoothed"], height=inc_std_average * 4)
+
+    x.loc[(objectID, peaks), "peak"] = 1
+    return x
+
+
+def dynamic_weighting(x: pd.DataFrame):
+    index = np.arange(x.shape[0])
+    peak_index: np.array = index[x["peak"] == 1]
+
+    # if we have 0 or just one peak we cannot calculate a period
+    if peak_index.size > 1:
+        # Calculate distance between consecutive peaks
+        distance = np.diff(peak_index)
+        # Determine expected period, may be a bit broke if we only have a few distances
+        period = int(np.median(distance))
+
+        x["peak_exp"] = x["peak"][::-1].rolling(window=period).max().bfill()[::-1]
+        x["peak_exp"] = x["peak_exp"].ewm(span=period).sum()
+    return x
+
+
+def add_engineered_peaks(data: pd.DataFrame) -> pd.DataFrame:
+    # already allocating features space
+    data["peak"] = 0.0
+    data["peak_exp"] = 0.0
+    data["smoothed"] = 0.0
+    # get peaks
+    data = data.groupby(level=0, group_keys=False).apply(get_peaks)
+    # weight peaks
+    data = data.groupby(level=0, group_keys=False).apply(dynamic_weighting)
+
+    return data
+
+
+def add_engineered_features(data: pd.DataFrame, features_in: List[str], engineered_features: dict) \
+        -> Tuple[pd.DataFrame, List[str]]:
+    existing_features = data.columns.tolist()
+    features = features_in.copy()
+
+    # type and some kind of lambda fnc (std, mean, etc.)
+    for (math_type, lambda_fnc), feature_list in engineered_features.items():
+        # add for all features that should get math_type
+        for feature in feature_list:
+            new_feature_name = feature + "_" + math_type
+            # only add if not already existing
+            if new_feature_name not in existing_features:
+                # groupby objectIDs, get a feature and then apply rolling window for each objectID, is returned as series
+                # and then added back to the DF
+                data[new_feature_name] = data.groupby(level=0, group_keys=False)[[feature]].apply(lambda_fnc).bfill()
+
+            # always append to features
+            features.append(new_feature_name)
+
+    return data, features
+
+
 def add_lag_features(df: pd.DataFrame, feature_cols: List[str], lag_steps: int):
     new_columns = pd.DataFrame({f"{col}_lag{i}": df.groupby(level=0, group_keys=False)[col].shift(i * 3)
                                 for i in range(1, lag_steps + 1)
@@ -97,35 +162,20 @@ def add_lag_features(df: pd.DataFrame, feature_cols: List[str], lag_steps: int):
     return df_out, features_out
 
 
-def add_EW_features(data: pd.DataFrame, features_in: List[str], window_size: int) -> Tuple[pd.DataFrame, List[str]]:
+def add_EW_features(data: pd.DataFrame, features_in: List[str]) -> Tuple[pd.DataFrame, List[str]]:
     features_ew = features_in.copy()
-
     # FEATURE ENGINEERING
-    for (math_type, lambda_fnc), feature_list in ENGINEERED_FEATURES_EW.items():
-        for feature in feature_list:
-            new_feature_name = feature + "_" + math_type + "_EW"
-            # groupby objectIDs, get a feature and then apply rolling window for each objectID, is returned as series
-            # and then added back to the DF
-            data[new_feature_name] = data.groupby(level=0, group_keys=False)[[feature]].apply(lambda_fnc).bfill()
-            features_ew.append(new_feature_name)
+    data, features_ew = add_engineered_features(data, features_ew, ENGINEERED_FEATURES_EW)
 
-    data, features_ew = add_lag_features(data, features_ew, 8)
     return data, features_ew
 
 
-def add_NS_features(data: pd.DataFrame, features_in: List[str], window_size: int) -> Tuple[pd.DataFrame, List[str]]:
+def add_NS_features(data: pd.DataFrame, features_in: List[str]) -> Tuple[pd.DataFrame, List[str]]:
     features_ns = features_in.copy()
 
     # FEATURE ENGINEERING
-    for (math_type, lambda_fnc), feature_list in ENGINEERED_FEATURES_NS.items():
-        for feature in feature_list:
-            new_feature_name = feature + "_" + math_type + "_NS"
-            # groupby objectIDs, get a feature and then apply rolling window for each objectID, is returned as series
-            # and then added back to the DF
-            data[new_feature_name] = data.groupby(level=0, group_keys=False)[[feature]].apply(lambda_fnc).bfill()
-            features_ns.append(new_feature_name)
+    data, features_ns = add_engineered_features(data, features_ns, ENGINEERED_FEATURES_NS)
 
-    data, features_ns = add_lag_features(data, features_ns, 8)
     return data, features_ns
 
 
@@ -151,52 +201,61 @@ def load_test_data_and_preprocess(filepath: Path) -> Tuple[pd.DataFrame, List[st
     merged_data[DEG_FEATURES] = np.unwrap(np.deg2rad(merged_data[DEG_FEATURES]))
 
     # create EW and NS dataframe and features
-    data, rf_features_ew = add_EW_features(merged_data, RF_BASE_FEATURES_EW, 6)
-    data, rf_features_ns = add_NS_features(data, RF_BASE_FEATURES_NS, 6)
-    data = data.bfill()
+    data, rf_features_ew = add_EW_features(merged_data, RF_BASE_FEATURES_EW)
+    data, rf_features_ns = add_NS_features(data, RF_BASE_FEATURES_NS)
 
-    # adding smoothing because of some FPs
-    new_feature_name = "Inclination (deg)" + "_" + "std"
-    data[new_feature_name] = data.groupby(level=0, group_keys=False)[["Inclination (deg)"]].apply(
-        lambda x: x.rolling(window=WINDOW_SIZE).std())
-    data[new_feature_name + "smoothed_1"] = data.groupby(level=0, group_keys=False)[[new_feature_name]].apply(
-        lambda x: x[::-1].ewm(span=100, adjust=True).sum()[::-1])
-    data[new_feature_name + "smoothed_2"] = data.groupby(level=0, group_keys=False)[[new_feature_name]].apply(
-        lambda x: x.ewm(span=100, adjust=True).sum())
-    data.bfill(inplace=True)
-    data.ffill(inplace=True)
-    data[new_feature_name + "_smoothed"] = (data[new_feature_name + "smoothed_1"] +
-                                            data[new_feature_name + "smoothed_2"]) / 2
-    rf_features_ew.append(new_feature_name)
-    rf_features_ns.append(new_feature_name)
-    rf_features_ew.append(new_feature_name + "_smoothed")
-    rf_features_ns.append(new_feature_name + "_smoothed")
+    # already allocating features space
+    data = add_engineered_peaks(data)
+    rf_features_ew.append("peak_exp")
+    rf_features_ns.append("peak_exp")
+
+    # add lag
+    data, rf_features_ew = add_lag_features(data, rf_features_ew, 8)
+    data, rf_features_ns = add_lag_features(data, rf_features_ns, 8)
+
+    # # adding smoothing because of some FPs
+    # new_feature_name = "Inclination (deg)" + "_" + "std"
+    # data[new_feature_name] = data.groupby(level=0, group_keys=False)[["Inclination (deg)"]].apply(
+    #     lambda x: x.rolling(window=WINDOW_SIZE).std())
+    # data[new_feature_name + "smoothed_1"] = data.groupby(level=0, group_keys=False)[[new_feature_name]].apply(
+    #     lambda x: x[::-1].ewm(span=100, adjust=True).sum()[::-1])
+    # data[new_feature_name + "smoothed_2"] = data.groupby(level=0, group_keys=False)[[new_feature_name]].apply(
+    #     lambda x: x.ewm(span=100, adjust=True).sum())
+    # data.bfill(inplace=True)
+    # data.ffill(inplace=True)
+    # data[new_feature_name + "_smoothed"] = (data[new_feature_name + "smoothed_1"] +
+    #                                         data[new_feature_name + "smoothed_2"]) / 2
+    # rf_features_ew.append(new_feature_name)
+    # rf_features_ns.append(new_feature_name)
+    # rf_features_ew.append(new_feature_name + "_smoothed")
+    # rf_features_ns.append(new_feature_name + "_smoothed")
 
     return data, rf_features_ew, rf_features_ns
 
 
 def classifier_preprocessing(df: pd.DataFrame, features_in: List[str]) -> Tuple[pd.DataFrame, List[str]]:
-    features_out = features_in.copy()
+    features = features_in.copy()
     # FEATURE ENGINEERING
-    for (math_type, lambda_fnc), feature_list in ENGINEERED_FEATURES_CLASSIFIER.items():
-        for feature in feature_list:
-            new_feature_name = feature + "_" + math_type
-            # groupby objectIDs, get a feature and then apply rolling window for each objectID, is returned as series
-            # and then added back to the DF
-            df[new_feature_name] = df.groupby(level=0, group_keys=False)[[feature]].apply(lambda_fnc).bfill()
-            features_out.append(new_feature_name)
 
-    return df, features_out
+    df, features = add_engineered_features(df, features, ENGINEERED_FEATURES_CLASSIFIER)
+
+    # Manually set the state change at timeindex 0
+    df["PREDICTED_FIRST_EW"] = 0
+    df["PREDICTED_FIRST_NS"] = 0
+    df.loc[df.loc[:, "TimeIndex"] == 0, "PREDICTED_FIRST_EW"] = 1
+    df.loc[df.loc[:, "TimeIndex"] == 0, "PREDICTED_FIRST_NS"] = 1
+
+    return df, features
 
 
-def changepoint_postprocessing(df: pd.DataFrame, window_size: int) -> pd.DataFrame:
+def changepoint_postprocessing(df: pd.DataFrame) -> pd.DataFrame:
     # post processing
-    df["PREDICTED_SUM_EW"] = df["PREDICTED_EW"].rolling(window_size, center=True).sum()
-    df["PREDICTED_SUM_NS"] = df["PREDICTED_NS"].rolling(window_size, center=True).sum()
+    df["PREDICTED_SUM_EW"] = df["PREDICTED_EW"].rolling(5, center=True).sum()
+    df["PREDICTED_SUM_NS"] = df["PREDICTED_NS"].rolling(3, center=True).sum()
     df["PREDICTED_CLEAN_EW"] = 0
     df["PREDICTED_CLEAN_NS"] = 0
     df.loc[df["PREDICTED_SUM_EW"] >= 5, "PREDICTED_CLEAN_EW"] = 1
-    df.loc[df["PREDICTED_SUM_NS"] >= 5, "PREDICTED_CLEAN_NS"] = 1
+    df.loc[df["PREDICTED_SUM_NS"] >= 3, "PREDICTED_CLEAN_NS"] = 1
 
     # removing two changepoints that are directly next to each other.
     diff = df["PREDICTED_CLEAN_EW"].shift(1, fill_value=0) & df["PREDICTED_CLEAN_EW"]
@@ -288,9 +347,9 @@ def generate_output(pred_ew: Tensor, pred_ns: Tensor, int_to_str_translation: di
 def main():
     start_time = time.perf_counter()
     # Load models for prediction
-    rf_ew = load(TRAINED_MODEL_DIR + "state_classifier_EW_HistBoosting.joblib")
-    rf_ns = load(TRAINED_MODEL_DIR + "state_classifier_NS_HistBoosting.joblib")
-    # models were trained with more than 4 cpus
+    rf_ew = load(TRAINED_MODEL_DIR + "state_classifier_EW_xgboost.joblib")
+    rf_ns = load(TRAINED_MODEL_DIR + "state_classifier_NS_xgboost.joblib")
+    # rf models were trained with more than 4 cpus
     # update_rf_params = {"n_jobs": 4}
     # rf_ew = rf_ew.set_params(**update_rf_params)
     # rf_ns = rf_ns.set_params(**update_rf_params)
@@ -318,18 +377,13 @@ def main():
     print(f"Time: {time.perf_counter() - start_time:4.0f} sec - States predicted")
     start_time = time.perf_counter()
 
-    # post-processing / pre-processing for classification
-    df = changepoint_postprocessing(df, 5)
+    # post-processing
+    df = changepoint_postprocessing(df)
 
     # CHANGEPOINTS DONE. NOW CLASSIFICAITON ----------------------------------------------------------------------------
     df, classifier_features = classifier_preprocessing(df, CLASSIFIER_FEATURES)
     # EW and NS have the same classification features
     df.loc[:, classifier_features] = scaler.transform(df.loc[:, classifier_features])
-    # Manually set the state change at timeindex 0
-    df["PREDICTED_FIRST_EW"] = 0
-    df["PREDICTED_FIRST_NS"] = 0
-    df.loc[df.loc[:, "TimeIndex"] == 0, "PREDICTED_FIRST_EW"] = 1
-    df.loc[df.loc[:, "TimeIndex"] == 0, "PREDICTED_FIRST_NS"] = 1
 
     ds_ew = SubmissionWindowDataset(df, classifier_features, "PREDICTED_CLEAN_EW", window_size=51)
     ds_ns = SubmissionWindowDataset(df, classifier_features, "PREDICTED_CLEAN_NS", window_size=101)
